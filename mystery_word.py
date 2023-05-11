abf5da4ca647e094531ac466254f86c892c8a8e1
qutaibaalani <qutaibaalani1984@gmail.com>
2023-05-11 10:19:19 -0400

    created 2 functinos to open and read the file and the other function to chose the random word

--- mystery_word.py
+++ mystery_word.py
@@ -1,3 +1,29 @@
+import random
+
+
+# This function is to open and to read the file
+def open_file(file):
+    # To open the file in read mode
+    with open(file) as my_file:
+        # To make the file readable
+        data = my_file.read()
+        # To put the words into a list
+        format_data = data.split()
+        # To catch the formatted data
+        return format_data
+
+
+# This function is to chose the random word
+def random_word(list):
+    # To select the random word from the blank list
+    word = random.choice(list)
+    # To catch the random word
+    return word
+
+
+print(word)
+
+
 def play_game():
     pass
 
